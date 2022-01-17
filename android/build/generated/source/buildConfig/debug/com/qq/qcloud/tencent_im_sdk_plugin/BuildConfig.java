3682990c70abe88c7f23fd5ba73bb4c6ae2ea72e
FanXiangDong <fanxiangdong1@haier.com>
2022-01-17 11:18:52 +0800

    init commit. copy from .pub-cache/hosted/pub.dartlang.org/tencent_im_sdk_plugin-3.6.7

---
+++ android/build/generated/source/buildConfig/debug/com/qq/qcloud/tencent_im_sdk_plugin/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qq.qcloud.tencent_im_sdk_plugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qq.qcloud.tencent_im_sdk_plugin";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.qq.qcloud.tencent_im_sdk_plugin";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
